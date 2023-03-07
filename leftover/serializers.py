4c908f0162755e1cebde37e65011f7cf3d24ddfa
Cindy Wong <cindywongdev@gmail.com>
2023-03-07 17:17:29 -0500

    added ListingSerializer

---
+++ leftover/serializers.py
@@ -0,0 +1,9 @@
+from .models import Listing
+from rest_framework import serializers
+
+# Listing serializer
+class ListingSerializer(serializers.HyperlinkedModelSerializer):
+    # the model it will serialize
+    model = Listing
+    # the fields that should be included in the serialized output
+    fields = ['id', 'food_name', 'listing_date', 'img', 'ingredients', 'allergens', 'good_for_x_days', 'num_servings', 'pickup_by_time', 'is_expired', 'is_out_of_food']
