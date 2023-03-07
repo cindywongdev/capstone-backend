635914737350b4ac44fba63316e5f3247b89742c
Cindy Wong <cindywongdev@gmail.com>
2023-03-07 17:42:25 -0500

    created viewset and router

--- capstoneproject/urls.py
+++ capstoneproject/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from leftover.views import ListingViewSet
+
+# create new router
+router = routers.DefaultRouter()
+# register our viewset(s)
+router.register(r'listings', ListingViewSet) #register /listings routers
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # add all of our router urls
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls)
 ]
--- leftover/views.py
+++ leftover/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Listing
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ListingSerializer
+
 
 # Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    # main query for index route
+    queryset = Listing.objects.all()
+    # serializer class for serializing output
+    serializer_class = ListingSerializer
+    # option permission class to set permission level
+    # Could be [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
