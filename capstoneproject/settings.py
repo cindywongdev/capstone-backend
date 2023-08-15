e944729a5a4919a016ae4f90ac0f0cc5f5ee6d2e
Cindy Wong <cindywongdev@gmail.com>
2023-08-14 20:19:35 -0400

    updated database host

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -85,7 +85,12 @@ WSGI_APPLICATION = 'capstoneproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     'default': dj_database_url.config(
+        default='postgresql://postgres:4wUAmXb8FGhCoTNn@db.zioahqtlhumqmzrssohz.supabase.co:5432/postgres',
         conn_max_age=600,
         conn_health_checks=True,
     )
